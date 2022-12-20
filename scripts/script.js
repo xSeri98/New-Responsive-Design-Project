6a66815e918b950ba51da7207274b76b9ec9474c
Jose Mari Ronquillo <ronquillo.236225@pasay-edsa.sti.edu.ph>
2022-12-20 17:04:12 +0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
-function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
+
+var btn = document.getElementById("emailBtn");
+var element = document.getElementById("emailBtn");
+var idOfElement = element.getAttribute("id");
+
+
+
+btn.onclick = function() {
+
+    document.getElementById(idOfElement).setAttribute('style', 'background: azure; color: black;');
+    setTimeout(afterClick, 50);
+
 }
+   
+function afterClick(){
+    document.getElementById('emailBtn').setAttribute('style', 'background: fdcb6e; color: azure');
+}
+

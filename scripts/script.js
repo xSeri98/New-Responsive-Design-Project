d05ca1995346087c94e9e9d4f61e4fbf6d11b549
wee25see <98908587+wee25see@users.noreply.github.com>
2022-12-22 07:20:04 +0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,17 @@ var licontent = ulcontent.children[0];
 
 console.log(licontent.childNodes[0].nodeType);
 
+//When download resume button is click this function will execute to display "Downloading..." inside the button then revert it to it's initial value after 2 seconds.
+var btn = document.getElementById("emailBtn");
+var btnTextInitial = btn.childNodes[0].nodeValue;
+btn.onclick = function(){
+
+    btn.childNodes[0].nodeValue = "Downloading..."
+
+    function afterClick(){
+
+        btn.childNodes[0].nodeValue = btnTextInitial;
+    }
+
+    setTimeout(afterClick,2000);
+}
